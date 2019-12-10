79b6c2a42c1dd8796b2779f003957ccaf3d5eaf3
zenthium <georgiajosh22@gmail.com>
2019-12-09 22:59:30 -0500

    add new projectcard

--- projects/TodoListApp/src/display.js
+++ projects/TodoListApp/src/display.js
@@ -1,21 +1,21 @@
-import { createTodoForm, projectsList } from "./index";
+import { createTodoForm, projectsList } from './index';
 
 function displayProjects(projectList) {
-  const projectContainer = document.querySelector("#projectDisplay");
+  const projectContainer = document.querySelector('#projectDisplay');
   while (projectContainer.children[2]) {
     projectContainer.removeChild(projectContainer.children[2]);
   }
   for (let i = 0; i < projectList.length; i++) {
-    let projectCard = document.createElement("div");
-    projectCard.classList.add("projectButton");
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('projectButton');
     projectCard.innerHTML = `${projectList[i].name} (${projectList[i].todos.length})`;
-    projectCard.addEventListener("click", e => {
+    projectCard.addEventListener('click', e => {
       try {
-        let currentlySelectedProject = document.getElementsByClassName(
-          "projectButton clicked"
+        const currentlySelectedProject = document.getElementsByClassName(
+          'projectButton clicked'
         );
-        currentlySelectedProject[0].classList.remove("clicked");
-        projectCard.classList.add("clicked");
+        currentlySelectedProject[0].classList.remove('clicked');
+        projectCard.classList.add('clicked');
       } catch (error) {
         projectCard.classList.add('clicked');
       }
@@ -25,8 +25,8 @@ function displayProjects(projectList) {
   }
 }
 function displayTodoItems(project) {
-  let todos = project.todos;
-  todos.sort(function(value1, value2) {
+  const todos = project.todos;
+  todos.sort((value1, value2) => {
     //Sort by dates
     //If the first item has a lower (earlier) date, move it up
     //If it has a higher (later) date, move it down
@@ -40,56 +40,56 @@ function displayTodoItems(project) {
     if (parseInt(value1.priority) > parseInt(value2.priority)) return 1;
   });
   project.todos = todos;
-  const todoItemContainer = document.querySelector("#todoItems");
-  todoItemContainer.innerHTML = "";
+  const todoItemContainer = document.querySelector('#todoItems');
+  todoItemContainer.innerHTML = '';
   // while (todoItemContainer.firstChild) {
   //   todoItemContainer.removeChild(todoItemContainer.firstChild);
   // }
   for (let i = 0; i < todos.length; i++) {
-    let todoCard = document.createElement("div");
-    todoCard.classList.add("todoCard");
+    const todoCard = document.createElement('div');
+    todoCard.classList.add('todoCard');
     todoCard.id = i;
-    let checkboxDiv = document.createElement("div");
-    checkboxDiv.id = "checkbox";
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+    const checkboxDiv = document.createElement('div');
+    checkboxDiv.id = 'checkbox';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
     checkboxDiv.appendChild(checkbox);
     todoCard.appendChild(checkboxDiv);
-    let nameDiv = document.createElement("p");
+    const nameDiv = document.createElement('p');
     nameDiv.innerHTML = `<strong>${todos[i].name}</strong>`;
     todoCard.appendChild(nameDiv);
-    let dateCreated = document.createElement("p");
+    const dateCreated = document.createElement('p');
     dateCreated.innerHTML = `<strong>Due Date: </strong> ${
       todos[parseInt(todoCard.id)].dueDate
     }`;
     todoCard.appendChild(dateCreated);
-    todoCard.addEventListener("click", e => {
+    todoCard.addEventListener('click', e => {
       if (todoCard.classList.length > 1) {
         todoCard.removeChild(todoCard.childNodes[3]);
         todoCard.removeChild(todoCard.childNodes[3]);
         todoCard.removeChild(todoCard.childNodes[3]);
         todoCard.classList.remove(todoCard.classList.item(1));
       } else {
-        todoCard.classList.add("expanded");
-        let priority = document.createElement("p");
+        todoCard.classList.add('expanded');
+        const priority = document.createElement('p');
         priority.innerHTML = `<strong>Priority: </strong> ${
           todos[parseInt(i)].priority
         }`;
         todoCard.appendChild(priority);
-        let deleteButton = document.createElement("button");
-        deleteButton.classList.add("controlButton", "delete");
-        deleteButton.innerHTML = "Remove Task";
-        deleteButton.addEventListener("click", e => {
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('controlButton', 'delete');
+        deleteButton.innerHTML = 'Remove Task';
+        deleteButton.addEventListener('click', e => {
           todoItemContainer.removeChild(todoItemContainer.children[i]);
           project.todos.splice(i, 1);
           displayTodoItems(project);
           displayProjects(projectsList);
         });
         todoCard.appendChild(deleteButton);
-        let editButton = document.createElement("button");
-        editButton.classList.add("controlButton", "edit");
-        editButton.innerHTML = "Edit Task";
-        editButton.addEventListener("click", e => {
+        const editButton = document.createElement('button');
+        editButton.classList.add('controlButton', 'edit');
+        editButton.innerHTML = 'Edit Task';
+        editButton.addEventListener('click', e => {
           createTodoForm(true, project, i);
         });
         todoCard.appendChild(editButton);
